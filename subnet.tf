3c8b04e25e233491ac6bceabcf25421343ff4bc3
Ganesh Kishor Shetty <shetty.ga@northeastern.edu>
2025-02-19 01:20:59 -0500

    pushing the required files

---
+++ subnet.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "tf-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "tf-private-subnet-${count.index + 1}"
+  }
+}
