1c5545070bd7201eb5958640712aeab57d47feed
Ganesh Kishor Shetty <shetty.ga@northeastern.edu>
2025-04-15 23:37:53 -0400

    testing

--- EC2.tf
+++ EC2.tf
@@ -5,9 +5,9 @@ resource "aws_security_group" "app_sg" {
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
     security_groups = [aws_security_group.lb_sg.id]
     # SSH access only from Load Balancer SG
     # cidr_blocks = ["0.0.0.0/0"]
